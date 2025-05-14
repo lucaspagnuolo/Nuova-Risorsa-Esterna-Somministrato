1d34e23662d8877de0ac8553c01acf56d3515ab8
lucaspagnuolo <luca.spagnuolo.ext@consip.it>
2025-05-14 12:44:49 +0200

    Update Risorsa_Esterna_somministrato.py

--- Risorsa_Esterna_somministrato.py
+++ Risorsa_Esterna_somministrato.py
@@ -124,7 +124,7 @@ company            = company
 # ------------------------------------------------------------
 # Anteprima Messaggio
 # ------------------------------------------------------------
-if st.button("Anteprima Messaggio"):
+if st.button("Template per Posta Elettronica"):
     sAM     = genera_samaccountname(nome, cognome, secondo_nome, secondo_cognome, True)
     cn      = build_full_name(cognome, secondo_cognome, nome, secondo_nome, True)
     exp_fmt = formatta_data(expire_date)
